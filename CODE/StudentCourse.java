f14e0a5af77a830c57bfa30306305140cb056b5d
xinlinkasagisaori <44395214+xinlinkasagisaori@users.noreply.github.com>
2018-11-02 15:54:13 +0800

    Update StudentCourse.java

--- CODE/StudentCourse.java
+++ CODE/StudentCourse.java
@@ -1,5 +1,20 @@
 import java.util.*;
 
 public class StudentCourse {
-
+	private Course aCourse;
+	private Grade aGrade;
+	private int anIndex;
+	
+	public StudentCourse(Course aCourse, int anIndex) {
+		this.aCourse = aCourse;
+		this.anIndex = anIndex;
+	}
+	
+	public String getCourseID() {
+		return this.aCourse.getCourseID();
+	}
+	public void addGrade(){
+		// haven't implement
+	}
 }
+
