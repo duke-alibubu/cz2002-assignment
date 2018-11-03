4443b47d9fa93b88b15a75c7fec2df96717754a2
xinlinkasagisaori <44395214+xinlinkasagisaori@users.noreply.github.com>
2018-11-04 00:12:16 +0800

    Create StudentCourse.java

---
+++ CODE/entities/StudentCourse.java
@@ -0,0 +1,31 @@
+package entities;
+ public class StudentCourse {
+	private Course aCourse;
+	private Grade aGrade;
+	private int anIndex;
+	
+	public StudentCourse(Course aCourse, int anIndex)
+	{
+		this.aCourse = aCourse;
+		this.anIndex = anIndex;
+	}
+	
+	public Course getCourse()
+	{
+		return this.aCourse;
+	}
+	public Grade getGrade()
+	{
+		return aGrade;
+	}
+	public boolean setGrade(String ComponentName, float Value)
+	{
+		return aGrade.editComponent(ComponentName, Value);
+	}
+	public int getIndex(){
+		return this.anIndex;
+	}
+	public void setIndex(int index){
+		this.anIndex = index;
+	}
+}
