1469b4064994538bf00da7ff30e85a58393ee2ac
duke-alibubu <44540906+duke-alibubu@users.noreply.github.com>
2018-11-07 01:18:42 +0800

    Update Component.java

--- CODE/entities/Component.java
+++ CODE/entities/Component.java
@@ -10,6 +10,10 @@ public class Component
 		this.Name = new String(Name);
 		this.Value = Value;
 	}
+	public Component(Component c) {
+		this.Name = new String(c.Name);
+		this.Value = c.Value;
+	}
 	public Component(String Name)
 	{
 		this.Name = new String(Name);
