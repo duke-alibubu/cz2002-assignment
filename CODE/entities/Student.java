960cb20f7665707d3195f3781a1cbd71f1e8474a
YuqianChan <44394383+YuqianChan@users.noreply.github.com>
2018-11-10 17:06:02 +0800

    Update Student.java

--- CODE/entities/Student.java
+++ CODE/entities/Student.java
@@ -3,6 +3,7 @@ import java.util.ArrayList;
 import java.io.Serializable;
 
 public class Student implements Serializable{
+	private static final long serialVersionUID = 1L;
 	private String StudentName;
 	private String StudentID;
 	private String Faculty;
