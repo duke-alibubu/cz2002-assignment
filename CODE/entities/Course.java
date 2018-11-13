8b7c0ef5cfb5f3d01ee6061ec2c7a1e383b57e25
joisse1101 <44112666+joisse1101@users.noreply.github.com>
2018-11-14 02:39:57 +0800

    Update CODE/entities/Course.java

--- CODE/entities/Course.java
+++ CODE/entities/Course.java
@@ -10,41 +10,41 @@ public class Course implements Serializable {
 	private String CourseCoordinatorName;
   	private ArrayList<Lecture> CourseLecture;
 	private Assessment CourseAssessment;
-	
+
 	public Course(String CourseID, String CourseName, String CourseCoordinator) {
 		CourseLecture = new ArrayList<Lecture>(); //instantiate the CourseLecture
 		CourseAssessment = new Assessment();
-		this.CourseID = new String(CourseID); 
-		this.CourseName = new String(CourseName); 
+		this.CourseID = new String(CourseID);
+		this.CourseName = new String(CourseName);
 		this.CourseCoordinatorName = new String (CourseCoordinator);}
-    
+
 	public Course() {
 		CourseLecture = new ArrayList<Lecture>(); //instantiate the CourseLecture
 		CourseAssessment = new Assessment();
 		}
-	
+
 	public String getCourseID() {return CourseID;}
-  
+
 	public void setCourseID(String CourseID) {this.CourseID = new String(CourseID);}
-  
+
 	public String getCourseName() {return CourseName;}
-  
+
 	public void setCourseName(String CourseName) {this.CourseName = new String(CourseName);}
-  
-	public String getCourseCoordinatoerName() {return CourseCoordinatorName;}
-  
+
+	public String getCourseCoordinatorName() {return CourseCoordinatorName;}
+
 	public void setCourseCoordinatorName(String CourseCoordinatorName) {this.CourseCoordinatorName = new String(CourseCoordinatorName);}
-	
+
 	public void addLecture(Lecture lecture1) {this.CourseLecture.add(lecture1);}
     //not used
   	public void removeLecture(Lecture lecture1) {this.CourseLecture.remove(lecture1);}
-    
+
 	public boolean addAssessment(String assessName , float weight) {return this.CourseAssessment.addComponent(assessName , weight);}
-    	
+
 	public boolean removeAssessment(String assessName) {return this.CourseAssessment.removeComponent(assessName);}
-	
+
 	public boolean editAssessment(String assessName, float weight) { return this.CourseAssessment.editComponent(assessName,weight); }
-	
+
 	public ArrayList<Lecture> getCourseLecture() {return CourseLecture;}
 
 	public Assessment getCourseAssessment() {return CourseAssessment;}
@@ -65,7 +65,7 @@ public class Course implements Serializable {
 		return false;
 	}
 	public boolean addStudenttoNoTutCourse(Student stud , Lecture lec) {
-		
+
 		for (Lecture lect : CourseLecture) {
 		 if (lect.getProfessorName().equals(lec.getProfessorName()))
 			return lec.addStudenttoLec(stud, -1);
@@ -77,7 +77,7 @@ public class Course implements Serializable {
 		for (Component AssessComp : CourseAssessment.Distribution) {
 			totalWeight += AssessComp.getValue();
 		}
-		if (totalWeight != 0) 
+		if (totalWeight != 0)
 			return 100/totalWeight ;
 		else return 0;
 	}
